6edebf61e3c19e99c70805f07a520aa76ad2427b
UmaShankar Avagadda <umagmrit@gmail.com>
2020-10-13 03:13:55 +0000

    changes for 9.0.0.p8

---
+++ zm-x-zimlet-nextcloud/scripts/postinst.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "*** Configuring Zimbra-Zimlet-Nextcloud ***"
+echo "*** Checking if Zimbra-Zimlet-Nextcloud zimlet is installed. ***"
+su - zimbra -c "zmmailboxdctl status"
+if [ $? -ne 0 ]; then
+   echo "*** Mailbox is not running... ***"
+   echo "*** Follow the steps below as zimbra user ignore if installing through install.sh .. ***"
+   echo "*** Install the Zimbra-Zimlet-Nextcloud zimlet. ***"
+   echo "*** zmzimletctl deploy /opt/zimbra/zimlets-network/zm-x-zimlet-nextcloud.zip ***"
+   echo "*** zmprov fc zimlet ***"
+else
+   echo "*** Deploying Zimbra-Zimlet-Nextcloud zimlet ***"
+   su - zimbra -c  "zmzimletctl deploy /opt/zimbra/zimlets-network/zm-x-zimlet-nextcloud.zip"
+   su - zimbra -c  "zmprov fc zimlet"
+fi
+echo "*** Zimbra-Zimlet-Nextcloud Installation Completed. ***"
+echo "*** Restart the mailbox service as zimbra user. Run ***"
+echo "*** su - zimbra ***"
+echo "*** zmmailboxdctl restart ***"
