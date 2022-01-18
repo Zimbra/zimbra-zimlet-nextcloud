d6db82bffb0511f62c9d2af44f12b50b1cf81268
Barry de Graaff <info@barrydegraaff.tk>
2022-01-18 14:13:53 +0100

    adding Spanish i18n

--- package.sh
+++ package.sh
@@ -2,4 +2,4 @@
 
 npm install
 zimlet build
-zimlet package -v 1.0.7 --zimbraXVersion ">=2.0.0" -n "zimbra-zimlet-nextcloud" --desc "Nextcloud integration for Zimbra" -l "Nextcloud Zimlet"
+zimlet package -v 1.0.8 --zimbraXVersion ">=2.0.0" -n "zimbra-zimlet-nextcloud" --desc "Nextcloud integration for Zimbra" -l "Nextcloud Zimlet"
