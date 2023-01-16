21e90e344522083e3253963192a5455547c6d217
UmaShankar Avagadda <umagmrit@gmail.com>
2023-01-16 11:58:29 +0000

    ZBUG-2722 : Deploy zimlet locally

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -6,7 +6,7 @@ if [ $? -ne 0 ]; then
    echo "*** Mailbox is not running... ***"
    echo "*** Follow the steps below as zimbra user ignore if installing through install.sh .. ***"
    echo "*** Install the Zimbra-Zimlet-Nextcloud zimlet. ***"
-   Iecho "*** zmzimletctl -l deploy /opt/zimbra/zimlets-network/zimbra-zimlet-nextcloud.zip ***"
+   echo "*** zmzimletctl -l deploy /opt/zimbra/zimlets-network/zimbra-zimlet-nextcloud.zip ***"
    echo "*** zmprov fc zimlet ***"
 else
    echo "*** Deploying Zimbra-Zimlet-Nextcloud zimlet ***"
